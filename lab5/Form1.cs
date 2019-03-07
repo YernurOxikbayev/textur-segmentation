9f9fbb104df37a5e296a84fb3213ceed03ad404a
YernurOxikbayev <ernur.oksikbaev@gmail.com>
2019-03-07 18:09:51 +0700

    Название дисциплины, удаление не нужных библиотек.

--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿//Дисциплина: Методы интеллектуальной обработки и анализа изображения
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
