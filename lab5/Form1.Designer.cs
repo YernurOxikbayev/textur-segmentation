95e28cf3a88d258bca35b19cf37c676bafdfe4d7
YernurOxikbayev <ernur.oksikbaev@gmail.com>
2019-03-07 17:59:12 +0700

    Добавьте файлы проекта.

---
+++ lab5/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Стандарт отклонение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.sigma_Segm_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(277, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "3-й момент";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.moment_Segm_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(435, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(708, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(370, 93);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 17;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(370, 122);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 18;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Порог";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Размер маски";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(983, 24);
+            this.menuStrip1.TabIndex = 24;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Исходное изображение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(785, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Два выделенных региона";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Сегментированное изображение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 442);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = " Текстурная сегментация";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab5/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmp;
+        private byte[] grayIm;
+        private bool[] binIm;
+        private int maskSize = 3;
+        private int threshold;
+        SegmentationFormula sf;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            getFile(pictureBox1);
+        }
+        public void getFile(PictureBox pictureBox1)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                int width = pictureBox1.Width;
+                int height = pictureBox1.Height;
+
+                bmp = new Bitmap(image, width, height);
+                Rgb2Gray();
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void Rgb2Gray()
+        {//Получаем высоту и ширину bitmap
+            int width = bmp.Width;
+            int height = bmp.Height;
+            //Блокируем биты исходного изображения в системной памяти
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            // общее количество байтов в изображении
+            int bytes = data.Stride * data.Height;
+            //Создание массивов байтов для хранения информации о пикселях изображения
+            byte[] pixelBuffer = new byte[bytes];
+            grayIm = new byte[bytes/4];
+            // Получаем адрес первых пиксельных данных
+            IntPtr first_pixel = data.Scan0;
+            // Копирование данных изображения в один из массивов байтов
+            Marshal.Copy(first_pixel, pixelBuffer, 0, bytes);
+            // Разблокируем бит из системной памяти так как у нас есть вся необходимая информация в массиве
+            bmp.UnlockBits(data);
+            // Перевод изображения в оттенки серого
+            float rgb = 0;
+            for (int i = 0; i < pixelBuffer.Length; i += 4)
+            {
+                rgb = pixelBuffer[i] * .21f;
+                rgb += pixelBuffer[i + 1] * .71f;
+                rgb += pixelBuffer[i + 2] * .071f;
+
+                grayIm[i/4] = (byte)rgb;
+            }
+        }
+
+        private Bitmap DefineTexture(int kernelSize)
+        {//Получаем высоту и ширину bitmap
+            int width = bmp.Width;
+            int height = bmp.Height;
+            //Создание массивов байтов для хранения информации о пикселях изображения
+            bool[] resultBw = new bool[width * height];
+            byte[] resultBuffer = new byte[width * height * 4];
+            //центральный пиксель смещен от границы ядра 
+            int filterOffset = (kernelSize - 1) / 2;
+            int calcOffset = 0;
+            int byteOffset = 0;
+            
+            float mean;
+            byte[] z = new byte[kernelSize * kernelSize];
+            int k;
+            double mu2;
+            double mu3;
+            double sigma;
+
+            for (int offsetY = filterOffset; offsetY < height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX < width - filterOffset; offsetX++)
+                {
+                    byteOffset = offsetY * width + offsetX;
+                    mean = 0;
+                    k = 0;
+                    mu2 = 0;
+                    mu3 = 0;
+                    sigma = 0;
+                    int[] hist = new int[256];
+                    int brightness;
+
+                    for (int filterY = -filterOffset;filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset;
+                            filterX <= filterOffset; filterX++)
+                        {
+                            calcOffset = (offsetY + filterY) * width + offsetX + filterX;
+
+                            brightness = grayIm[calcOffset];
+                            hist[brightness]++;//гистограмма
+                            z[k++] = grayIm[calcOffset];//яркость
+                            mean += grayIm[calcOffset];//суммируем яркостей каждого пикселя.
+                        }
+                    }
+
+                    mean = mean / (kernelSize * kernelSize);// среднее значение яркости изображения
+
+                    if (sf.Equals(SegmentationFormula.ThirdMoment))//Третий момент
+                    {
+                        for (int i = 0; i < k; i++)
+                        {
+                            mu3 += Math.Pow((z[i] - mean), 3) * hist[z[i]];
+                        }
+
+                        resultBw[byteOffset] = mu3 > threshold;
+                    }
+                    else if (sf.Equals(SegmentationFormula.Sigma))// стандартное отклонение
+                    {
+                        for (int i = 0; i < k; i++)
+                        {
+                            mu2 += Math.Sqrt((Math.Pow((z[i] - mean), 2)));
+                        }
+
+                        sigma = Math.Sqrt(mu2) / kernelSize;
+                        resultBw[byteOffset] = sigma > threshold;
+                    }
+
+                    resultBuffer[byteOffset * 4] = resultBw[byteOffset] ? (byte)255 : (byte)0;
+                    resultBuffer[byteOffset * 4 + 1] = resultBuffer[byteOffset * 4];
+                    resultBuffer[byteOffset * 4 + 2] = resultBuffer[byteOffset * 4];
+                    resultBuffer[byteOffset * 4 + 3] = 255;
+                }
+            }
+
+            binIm = resultBw;
+
+            Bitmap resultBitmap = new Bitmap(width, height);
+            //Блокируем биты исходного изображения в системной памяти
+            BitmapData resultData =
+                       resultBitmap.LockBits(new Rectangle(0, 0,
+                       resultBitmap.Width, resultBitmap.Height),
+                       ImageLockMode.WriteOnly,
+                       PixelFormat.Format32bppArgb);
+            // Копирование данных изображения в один из массивов байтов
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0,
+                                       resultBuffer.Length);
+            // Разблокируем бит из системной памяти так как у нас есть вся необходимая информация в массиве
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+        //стандартное отклонение
+        private void sigma_Segm_Click(object sender, EventArgs e)
+        {
+            threshold = 2;
+            numericUpDown2.Value = threshold;
+            sf = SegmentationFormula.Sigma;
+            pictureBox3.Image = DefineTexture(maskSize);
+            pictureBox2.Image = ShowSegmentedResult();
+        }
+        // 3-й момент 
+        private void moment_Segm_Click(object sender, EventArgs e)
+        {
+            threshold = 210;
+            numericUpDown2.Value = threshold;
+            sf = SegmentationFormula.ThirdMoment;
+            pictureBox3.Image = DefineTexture(maskSize);
+            pictureBox2.Image = ShowSegmentedResult();
+        }
+
+        private Image ShowSegmentedResult()
+        {
+            int width = bmp.Width;
+            int height = bmp.Height;
+            //Блокируем биты исходного изображения в системной памяти
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            int bytes = data.Stride * data.Height;
+
+            byte[] pixelBuffer = new byte[bytes];
+            // Копирование данных изображения в один из массивов байтов
+            Marshal.Copy(data.Scan0, pixelBuffer, 0, bytes);
+            // Разблокируем бит из системной памяти так как у нас есть вся необходимая информация в массиве
+            bmp.UnlockBits(data);
+
+            for (int i = 0; i < pixelBuffer.Length; i += 4)
+            {
+                if (!binIm[i/4])
+                {
+                    pixelBuffer[i] = 0;
+                    pixelBuffer[i + 1] = 0;
+                    pixelBuffer[i + 2] = 0;
+                }
+            }
+
+            Bitmap resultBitmap = new Bitmap(width, height);
+            //Блокируем биты исходного изображения в системной памяти
+            BitmapData resultData =resultBitmap.LockBits(new Rectangle(0, 0,resultBitmap.Width, resultBitmap.Height),
+                       ImageLockMode.WriteOnly,PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(pixelBuffer, 0, resultData.Scan0,pixelBuffer.Length);
+
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            maskSize = (int)numericUpDown1.Value;
+            pictureBox3.Image = DefineTexture(maskSize);
+            pictureBox2.Image = ShowSegmentedResult();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            threshold = (int)numericUpDown2.Value;
+            pictureBox3.Image = DefineTexture(maskSize);
+            pictureBox2.Image = ShowSegmentedResult();
+        }
+
+        public enum SegmentationFormula
+        {
+            ThirdMoment,
+            Sigma
+        }
+    }
+}
